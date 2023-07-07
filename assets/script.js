2d99d99c0258a7efb8176f0fd68764b0a3f16284
Josh Sandquist <joshdsand@gmail.com>
2023-07-07 10:07:44 -0700

    added function to retrieve city coordinates

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,31 @@ document.getElementById('search-form').addEventListener('submit', function(event
     // Passing in city name to getCoordinates function
     getCoordinates(city);
 });
+
+function getCoordinates(city) {
+    //creating API search using user inputted city and api key
+    var searchAPI = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKey}`
+    fetch(searchAPI)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        //returning the lattitude and longitude data from our search to be used in future function
+        if (data.length > 0) {
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            getWeather(lat,lon)
+        }
+        else {
+            alert('City not found!')
+        }
+    })
+    // catch block for error handling
+    .catch(function(error) {
+        alert('Error:' + error)
+    })
+}
+
+function getWeather(lat, lon) {
+    var weatherAPI =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+}
